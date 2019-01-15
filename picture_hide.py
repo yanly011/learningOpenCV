e79d3a334cae4458fd54d72adf039e008297b9d8
Lujing Yang <yanly011@gmail.com>
2019-01-15 20:58:53 +1100

    马赛克化

--- picture_hide.py
+++ picture_hide.py
@@ -5,10 +5,14 @@ img = cv2.imread('test.png',cv2.IMREAD_COLOR)
 print(height, width, deep) #(512, 512, 3)
 
 #将[200, 300]到[300,400]内的区域马赛克化
-(b, g, r)=img[200, 300]
-for i in range (200, 300):
+block_size = 10
+
+(b, g, r)=img[200, 400]
+for i in range (200, 500):
     for j in range(300, 400):
-        img[i, j] = (b, g, r)
+        if(i%20!=0 or j%20!=0):
+            img[i, j] = img[int(i/20) * 20, int(j/20) * 20]
+
 
 #显示结果：
 cv2.imshow('img',img)
